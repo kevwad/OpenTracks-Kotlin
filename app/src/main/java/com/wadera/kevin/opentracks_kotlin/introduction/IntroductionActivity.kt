6fa3952064ae2a38f4c52b9821165caa3b5d888d
kevwad <kevin.wadera.ca@gmail.com>
2024-05-11 15:45:10 -0400

    GH-1: Create initial commit with IntroductionActivity

---
+++ app/src/main/java/com/wadera/kevin/opentracks_kotlin/introduction/IntroductionActivity.kt
@@ -0,0 +1,12 @@
+package com.wadera.kevin.opentracks_kotlin.introduction
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.wadera.kevin.opentracks_kotlin.R
+
+class IntroductionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_introduction)
+    }
+}
